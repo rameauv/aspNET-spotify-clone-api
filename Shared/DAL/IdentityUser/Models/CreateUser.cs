82eca435378c0d8b4d302edc08fc0d49cabeeda5
valentin rameau <valentin.rameau@outlook.fr>
2023-01-14 17:23:49 +0900

    feat(default-profile-name): set a default profile name at registration

--- Shared/DAL/IdentityUser/Models/CreateUser.cs
+++ Shared/DAL/IdentityUser/Models/CreateUser.cs
@@ -1,7 +1,11 @@
+using Spotify.Shared.DAL.User.Models;
+
 namespace Spotify.Shared.DAL.IdentityUser.Models;
 
-public record CreateUser(string UserName, string Password)
+public record CreateUser(string UserName, string Password, UserData data)
 {
     public string UserName { get; set; } = UserName;
     public string Password { get; set; } = Password;
+
+    public UserData Data { get; set; } = data;
 }
