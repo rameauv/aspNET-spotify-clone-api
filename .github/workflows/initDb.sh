77eb8aff2d515a12ae326a7eff64a051063d559b
valentin rameau <valentin.rameau@outlook.fr>
2023-01-17 01:04:02 +0900

    feat(system-tests): ci

--- .github/workflows/initDb.sh
+++ .github/workflows/initDb.sh
@@ -1,6 +1,48 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get -y install postgresql
-psql postgresql://<user>:<password>@<host>/<db> << EOF
-       <your sql queries go here>
+psql postgresql://postgres:mysecretpassword@localhost:5432/postgres << EOF
+       create table "Users"
+       (
+       	"Id" uuid default gen_random_uuid() not null
+       		constraint "PK_Users"
+       			primary key,
+       	"UserName" text not null,
+       	"PasswordHash" text not null,
+       	"Data" jsonb
+       );
+       
+       alter table "Users" owner to postgres;
+       
+       create unique index users_username_uindex
+       	on "Users" ("UserName");
+       
+       create table "Likes"
+       (
+       	"Id" uuid default gen_random_uuid() not null
+       		constraint likes_pk
+       			primary key,
+       	"AssociatedId" text not null,
+       	"AssociatedUser" text not null,
+       	"AssociatedType" text
+       );
+       
+       alter table "Likes" owner to postgres;
+       
+       create unique index likes_id_uindex
+       	on "Likes" ("Id");
+       
+       create table "RefreshTokens"
+       (
+       	"Id" text default gen_random_uuid() not null
+       		constraint refreshtokens_pk
+       			primary key,
+       	"Token" text not null,
+       	"UserId" text not null
+       );
+       
+       alter table "RefreshTokens" owner to postgres;
+       
+       create unique index refreshtokens_id_uindex
+       	on "RefreshTokens" ("Id");
 EOF
