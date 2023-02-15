cf734c86396624b66a03eecabc4261c8d483a0ba
rameauv <valentin.rameau@outlook.fr>
2023-02-15 18:59:25 +0900

    feat(my-controller-base): documentation (#19)

--- API/Controllers/MyControllerBase.cs
+++ API/Controllers/MyControllerBase.cs
@@ -5,6 +5,9 @@ using Spotify.Shared.BLL.Jwt;
 
 namespace Api.Controllers;
 
+/// <summary>
+/// Base class for controller that contain usefully methods
+/// </summary>
 public class MyControllerBase : ControllerBase
 {
     private readonly IJwtService _jwtService;
@@ -14,12 +17,21 @@ public class MyControllerBase : ControllerBase
         _jwtService = jwtService;
     }
 
+    /// <summary>
+    /// Generate a new error response
+    /// </summary>
+    /// <param name="error"></param>
+    /// <returns></returns>
     protected IActionResult Error(ErrorDto error)
     {
         Response.ContentType = "application/problem+json";
         return new ErrorResult(error);
     }
 
+    /// <summary>
+    /// get the current access token
+    /// </summary>
+    /// <returns>return the access token or null if there is currently no access token</returns>
     protected string? GetAccessToken()
     {
         // Remove "Bearer "
@@ -32,19 +44,24 @@ public class MyControllerBase : ControllerBase
         return Request.Headers.Authorization.ToString()[7..];
     }
 
+    /// <summary>
+    /// get the current user's id
+    /// </summary>
+    /// <returns>return the current user id</returns>
+    /// <exception cref="InvalidOperationException">throw an new instance of <see cref="InvalidOperationException"/> if there is currently no access token or if the token does not contain the current user's id</exception>
     protected string GetCurrentUserId()
     {
         var accessToken = GetAccessToken();
         if (accessToken == null)
         {
-            throw new Exception("no access token provided");
+            throw new InvalidOperationException("no access token provided");
         }
 
         var validatedToken = _jwtService.GetValidatedAccessToken(accessToken);
         var userId = validatedToken.Principal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         if (userId == null)
         {
-            throw new ArgumentException("no userid in this access token");
+            throw new InvalidOperationException("no userid in this access token");
         }
 
         return userId;
