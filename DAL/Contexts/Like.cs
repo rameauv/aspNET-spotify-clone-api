7e26ab43c8ea2d9f76565a40677edbc3b750f2aa
valentin rameau <valentin.rameau@outlook.fr>
2023-02-26 02:05:37 +0900

    feat(library)

---
+++ DAL/Contexts/Like.cs
@@ -0,0 +1,14 @@
+﻿namespace Repositories.Contexts;
+
+public partial class Like
+{
+    public Guid Id { get; set; }
+
+    public string AssociatedId { get; set; } = null!;
+
+    public string AssociatedUser { get; set; } = null!;
+
+    public string AssociatedType { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+}
---
+++ DAL/Contexts/SpotifyContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Repositories.Contexts;
+
+public partial class SpotifyContext : DbContext
+{
+    public SpotifyContext(DbContextOptions<SpotifyContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Like> Likes { get; set; }
+
+    public virtual DbSet<RefreshToken> RefreshTokens { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasPostgresExtension("Spotify", "uuid-ossp");
+
+        modelBuilder.Entity<Like>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("likes_pk");
+
+            entity.HasIndex(e => e.Id, "likes_id_uindex").IsUnique();
+
+            entity.Property(e => e.Id).HasDefaultValueSql("gen_random_uuid()");
+            entity.Property(e => e.CreatedAt).HasColumnType("timestamp without time zone");
+        });
+
+        modelBuilder.Entity<RefreshToken>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("refreshtokens_pk");
+
+            entity.HasIndex(e => e.Id, "refreshtokens_id_uindex").IsUnique();
+
+            entity.Property(e => e.Id).HasDefaultValueSql("gen_random_uuid()");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasIndex(e => e.UserName, "users_username_uindex").IsUnique();
+
+            entity.Property(e => e.Id).HasDefaultValueSql("gen_random_uuid()");
+            entity.Property(e => e.Data).HasColumnType("jsonb");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
